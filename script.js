52449e2c3dc1343fd2ed46e39daa60120262d17d
Ayush Mishra <Ayushmishra0228@outlook.com>
2023-12-17 02:26:58 +0530

    Redesigned @ayush2802

--- script.js
+++ script.js
@@ -1,46 +1,32 @@
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#polina button");
 
-const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city;
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': 'cc515729b4msh967ac529fed8300p1d70c7jsn3c676d99f64e',
-        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-    }
-};
-
-const getWeather = (city) => 
-   { cityName.innerHTML = city
-    const response =  fetch(url, options);
-    const result = response.text();
-            {
-
-            //Cloud_pct.innerHTML = reponse.Cloud_pct
-            feels_like.innerHTML = reponse.feels_like
-            max_temp.innerHTML = reponse.max_temp
-            min_temp.innerHTML = reponse.min_temp
-            humidity.innerHTML = reponse.humidity
-            humidity2.innerHTML = reponse.humidity2
-            sunset.innerHTML = reponse.sunset
-            sunrise.innerHTML = reponse.sunrise
-            wind_degrees.innerHTML = reponse.wind_degrees
-            temp.innerHTML = reponse.temp
-            temp2.innerHTML = reponse.temp2
-            weather_id.innerHTML = reponse.weather_id
-            wind_speed.innerHTML = reponse.wind_speed
-            wind_degrees2.innerHTML = reponse.wind_degrees2
-            
-        
-   }
-    console.log(result)
-    .catch(error = console.error(error));
-
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+    vid.removeAttribute("autoplay");
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
 }
-    submit.addEventListener("click", (e) => {
-        e.preventDefault()
-        getWeather(city.value)
-    })
 
-    getWeather("Delhi")
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
 
 
-    
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})
