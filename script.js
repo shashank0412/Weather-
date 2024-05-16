5ae288e467936218308e4d7785d9e3431e796747
Ayushh <ayushmishra0228@outlook.com>
2024-05-17 01:55:04 +0530

    api injestion by ayushh

--- script.js
+++ script.js
@@ -5,30 +5,35 @@ const option ={
     "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
   }
 };
-
 const getWeather = (city) => {
-	cityName.innerHTML = city;
-	fetch("https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=delhi", option)
+	city_name.innerHTML = city;
+	fetch("https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city="+ city, option)
 	.then(response => response.json())
 	.then((response) => {
 		console.log(response);
 
-		cloud_pct.innerHTML = response.cloud_pct
-		temp.innerHTML = response.temp
-		feels_like.innerHTML = response. feels_like
-		humidity.innerHTML = response.humidity
-		min_temp.innerHTML = response.min_temp
-		max_temp.innerHTML = response.max_temp
-		wind_speed.innerHTML = response.wind_speed
-		wind_degrees.innerHTML = response.wind_degrees
-		sunrise.innerHTML = response.sunrise
-		sunset.innerHTML = response.sunset
+		temp.innerHTML	= response.temp;
+		temp2.innerHTML	= response.temp;
+		max_temp.innerHTML = response.max_temp;
+		min_temp.innerHTML = response.min_temp;
+
+		cloud_pct.innerHTML = response.cloud_pct;
+		feels_like.innerHTML =	response.feels_like;
+		humidity.innerHTML = response.humidity;
+		humidity2.innerHTML = response.humidity;
+		
+		sunrise.innerHTML = response.sunrise;
+		sunset.innerHTML = response.sunset;
+		wind_degrees.innerHTML = response.wind_degrees;
+		wind_speed.innerHTML = response.wind_speed;
+		wind_speed2.innerHTML = response.wind_speed;
 	})
 	.catch(err => {console.log(err)});
 }
-
-submit.addEventListener("click", (city) => {
-	getWeather(city.value);
-});
-
-getWeather("delhi");
+window.onload = function(){
+	submit.addEventListener("click", (e) => {
+		e.preventDefault();
+		getWeather(city.value);
+	});
+    getWeather("Delhi");
+}
