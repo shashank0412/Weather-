c7912afc24422482d700e2ff95950f0311a6c0e4
Ayushh <ayushmishra0228@outlook.com>
2024-05-16 23:52:32 +0530

    data injestion

---
+++ script.js
@@ -0,0 +1,25 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "ce580959b4msh04ac7f304c1ee24p143e88jsncf6cf022f1d6",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
+};
+
+fetch("https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=delhi", options)
+.then(response => response.json())
+.then((response) => {
+	console.log(response);
+
+	cloud_pct.innerHTML = response.cloud_pct
+    temp.innerHTML = response.temp
+    feels_like.innerHTML = response. feels_like
+    humidity.innerHTML = response.humidity
+    min_temp.innerHTML = response.min_temp
+    max_temp.innerHTML = response.max_temp
+    wind_speed.innerHTML = response.wind_speed
+    wind_degrees.innerHTML = response.wind_degrees
+    sunrise.innerHTML = response.sunrise
+    sunset.innerHTML = response.sunset
+})
+.catch(err => {console.log(err)});
